e7b61e8479603407b8e296392309ebffbd59a911
Yura Shinkarev <yshinkarev@gmail.com>
2018-05-17 00:35:40 +0300

    update pigz-directory.sh
    add argument --prefix to script

--- pigz-directory.sh
+++ pigz-directory.sh
@@ -5,24 +5,31 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-dirName=$(basename "$1")
-dir=$1
+DIRNAME=$(basename "$1")
+DIR=$1
 shift 1
 
-if [ ! -d "$dir" ]; then
+if [ ! -d "$DIR" ]; then
 	>&2 echo "No such directory"
 	exit 1
 fi
 
-excludeSize=
+EXCLUDESIZE=
+PREFIX=
 
 for arg in "$@"; do
 	  case $arg in
 	    --directory=*)
-	    	targetDir=${arg#*=}
+	    	TARGETDIR=${arg#*=}
+	    	shift
 	    	;;
 	    --exclude-size=*)
-		excludeSize=${arg#*=}
+		EXCLUDESIZE=${arg#*=}
+		shift
+	    	;;
+	    --prefix=*)
+		PREFIX=${arg#*=}
+		shift
 	    	;;
 	    *)
 	      >&2 echo "Unknown argument: $arg"
@@ -31,17 +38,19 @@ for arg in "$@"; do
 	  esac
 	done
 
-if [ -z "$targetDir" ]; then
-	targetDir=.
-else
-	shift 1
+if [ -z "$TARGETDIR" ]; then
+	TARGETDIR=.
 fi
 
-targetPath=$targetDir/$dirName-$(date +"%Y.%m.%d_%H-%M-%S").tar.gz
-echo "$dir -> $targetPath"
+if [ -z "$PREFIX" ]; then
+	PREFIX=$DIRNAME-
+fi
 
-if [ -z "$excludeSize" ]; then
-	tar hcf - "$dir" | /home/schumi/bin/compress/pigz -9 --quiet --keep --recursive --rsyncable $@ - > $targetPath
+targetPath=$TARGETDIR/$PREFIX$(date +"%Y.%m.%d_%H-%M-%S").tar.gz
+echo "$DIR -> $targetPath"
+
+if [ -z "$EXCLUDESIZE" ]; then
+	tar hcf - "$DIR" | /home/schumi/bin/compress/pigz -9 --quiet --keep --recursive --rsyncable $@ - > $targetPath
 else
-	tar hcf - "$dir" --exclude-from <(find -L $dir -size $excludeSize) | /home/schumi/bin/compress/pigz -9 --quiet --keep --recursive --rsyncable - > $targetPath
+	tar hcf - "$DIR" --exclude-from <(find -L $DIR -size $EXCLUDESIZE) | /home/schumi/bin/compress/pigz -9 --quiet --keep --recursive --rsyncable - > $targetPath
 fi
