73e42ed148fe440a5700771ebadb15d3393de42f
Yura Shinkarev <cfb1e84638ec2287fd1d6743cd317140@yshinkarev.ru>
2018-06-04 11:51:12 +0300

    add script pg_table_show_columns.sh

---
+++ pg_table_show_columns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# https://dev.mysql.com/doc/refman/5.7/en/resetting-permissions.html
+
+TABLE_NAME=$1
+if [ -z "$TABLE_NAME" ]; then
+	>&2 echo "Missing argument: [TABLE NAME]"
+	exit 1
+fi
+shift 1
+
+psql -Ac "select * from $TABLE_NAME where false;" $@ | head -n 1
