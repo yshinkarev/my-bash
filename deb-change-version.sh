ab422fdaff082b47ffd27f72fe95d1b1b04214b8
Yura Shinkarev <cfb1e84638ec2287fd1d6743cd317140@yshinkarev.ru>
2018-06-22 15:08:08 +0300

    add script deb-change-version.sh

---
+++ deb-change-version.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DEBFILE=$1
+if [[ -z "$DEBFILE" ]]; then
+	>&2 echo "Missing argument deb-file"
+	exit 1
+fi
+
+NEW_VERSION=$2
+if [[ -z "$NEW_VERSION" ]]; then
+	>&2 echo "Missing argument new version"
+	exit 1
+fi
+
+set -e
+function cleanup {
+	rm -rf "$TMPDIR"	
+}
+trap cleanup EXIT
+
+TMPDIR=`mktemp -d /tmp/deb.XXXXXXXXXX` || exit 1
+OUTPUT=$DEBFILE.modified
+
+if [[ -e "$OUTPUT" ]]; then
+	>&2 echo "$OUTPUT exists"
+	exit 1
+fi
+
+dpkg-deb -x "$DEBFILE" "$TMPDIR"
+dpkg-deb --control "$DEBFILE" "$TMPDIR"/DEBIAN
+
+if [[ ! -e "$TMPDIR"/DEBIAN/control ]]; then
+	>&2 echo "DEBIAN/control not found"
+	exit 1
+fi
+
+CONTROL="$TMPDIR"/DEBIAN/control
+sed -i "s/Version:.*/Version: $NEW_VERSION/g" $CONTROL
+
+dpkg -b "$TMPDIR" "$OUTPUT"
+echo "New file $OUTPUT generated"
