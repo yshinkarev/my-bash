7ee5504a0d46044dd3aa60b17cf66b673fd281b9
Yura Shinkarev <yshinkarev@gmail.com>
2019-02-11 11:40:55 +0300

    minor update

--- android_screencap.sh
+++ android_screencap.sh
@@ -5,6 +5,27 @@ TO_JPG=0
 NAME=
 ADD_TIMESTAMP=0
 
+########################################
+
+showHelp() {
+cat << EOF
+Usage: $(basename $0) [--directory=DIR] [--mask=MASK] [--format=FORMAT] [--help]
+Rename all files with mask MASK in directory DIR to file name with timestamp FORMAT
+
+  --directory   Directory to search
+  --mask        Files mask
+  --format      Timestamp format
+  --help        Show this help and exit
+EOF
+}
+
+########################################
+
+if [[ "$@" == *"--help"* ]]; then
+	showHelp
+	exit 0
+fi
+
 for arg in "$@"; do
 	  case $arg in
 	    --directory=*)
@@ -18,7 +39,7 @@ for arg in "$@"; do
 	    	;;
 	    --add-timestamp|-at)
 	    	ADD_TIMESTAMP=1
-	    	;;	    	
+	    	;;
 	    *)
 	      >&2 echo "Unknown argument: $arg"
 	      exit 1
@@ -29,7 +50,7 @@ for arg in "$@"; do
 if [ -z "$NAME" ]; then
 	NAME=$(adb shell getprop ro.product.model)
 	NAME=${NAME::-1}
-	NAME=$(echo "${NAME//[$'\t\r\n ']/-}")	
+	NAME=$(echo "${NAME//[$'\t\r\n ']/-}")
 fi
 
 PTH=$DIR/$NAME
@@ -44,7 +65,7 @@ adb shell screencap -p | sed 's/\r$//' > $FILE_NAME
 FINAL_FILE_NAME=$FILE_NAME
 if [ $TO_JPG == 1 ]; then
 	FINAL_FILE_NAME="$PTH".jpg
-	convert $FILE_NAME $FINAL_FILE_NAME	
+	convert $FILE_NAME $FINAL_FILE_NAME
 	rm $FILE_NAME
 fi
 
--- pg_get_connections.sh
+++ pg_get_connections.sh
@@ -6,4 +6,4 @@ if [ -z $DB_NAME ]; then
 	exit 1
 fi
 
-psql --pset="pager=off" --pset="footer=off" --pset="border=2" --pset="format=wrapped" -c "SELECT pid, usename, application_name, client_addr, client_port, date_trunc('second', backend_start) AS backend_start, wait_event, state, LEFT(query, 40) AS query FROM pg_stat_activity WHERE datname = '$DB_NAME'"
+psql --pset="pager=off" --pset="footer=off" --pset="border=2" --pset="format=wrapped" -c "SELECT pid, usename, application_name, client_addr, client_port, date_trunc('second', backend_start) AS backend_start, wait_event, state, LEFT(query, 80) AS query FROM pg_stat_activity WHERE datname = '$DB_NAME'"
