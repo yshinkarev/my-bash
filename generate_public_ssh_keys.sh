c25fc54e336f3445e999c501c7e14349cf92bc4b
Yura Shinkarev <yshinkarev@gmail.com>
2019-08-30 15:49:25 +0300

    add script generate_public_ssh_keys.sh

---
+++ generate_public_ssh_keys.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PUBLIC_DIR=public
+mkdir -p ${PUBLIC_DIR}
+
+OIFS="$IFS"
+IFS=$'\n'
+for FILE in $(find . -type f 2>/dev/null); do
+    ssh-keygen -y -f ${FILE} > ${PUBLIC_DIR}/"${FILE}".pub
+done
+IFS="$OIFS"
