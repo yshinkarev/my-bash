f823638de0022bfea6137d282d849937577394ab
Yura Shinkarev <yshinkarev@gmail.com>
2019-12-25 09:08:46 +0300

    update android_screencap script

--- android_screencap.sh
+++ android_screencap.sh
@@ -8,65 +8,73 @@ ADD_TIMESTAMP=0
 ########################################
 
 showHelp() {
-cat << EOF
-Usage: $(basename $0) [--directory=DIR] [--mask=MASK] [--format=FORMAT] [--help]
-Rename all files with mask MASK in directory DIR to file name with timestamp FORMAT
+    cat <<EOF
+Usage: $(basename $0) [OPTIONS]
+Make screenshot from android device using screencap
 
-  --directory   Directory to search
-  --mask        Files mask
-  --format      Timestamp format
-  --help        Show this help and exit
+  -d,  --directory      Output directory
+  -n,  --name           Output filename
+  -tj, --to-jpg         Auto convert screenshot to jpeg format
+  -at, --add-timestamp  Add filename suffix with current timestamp
+  -h,  --help           Show this help and exit
 EOF
 }
 
 ########################################
 
-if [[ "$@" == *"--help"* ]]; then
-	showHelp
-	exit 0
+if [[ "$@" == *"--help"* ]] || [[ "$@" == *"-h"* ]]; then
+    showHelp
+    exit 0
 fi
 
 for arg in "$@"; do
-	  case $arg in
-	    --directory=*)
-	    	DIR=${arg#*=}
-	    	;;
-	    --name=*)
-	    	NAME=${arg#*=}
-	    	;;
-	    --to-jpg|-tj)
-	    	TO_JPG=1
-	    	;;
-	    --add-timestamp|-at)
-	    	ADD_TIMESTAMP=1
-	    	;;
-	    *)
-	      >&2 echo "Unknown argument: $arg"
-	      exit 1
-	      ;;
-	  esac
-	done
+    case $arg in
+    --directory=*)
+        DIR=${arg#*=}
+        ;;
+    -d=*)
+        DIR=${arg#*=}
+        ;;
+    -n=*)
+        NAME=${arg#*=}
+        ;;
+    --name=*)
+        NAME=${arg#*=}
+        ;;
+    --to-jpg | -tj)
+        TO_JPG=1
+        ;;
+    --add-timestamp | -at)
+        ADD_TIMESTAMP=1
+        ;;
+    *)
+        echo >&2 "Unknown argument: $arg"
+        exit 1
+        ;;
+    esac
+done
 
 if [ -z "$NAME" ]; then
-	NAME=$(adb shell getprop ro.product.model)
-	NAME=${NAME::-1}
-	NAME=$(echo "${NAME//[$'\t\r\n ']/-}")
+    NAME=$(adb shell getprop ro.product.model)
+    NAME=${NAME::-1}
+    NAME=$(echo "${NAME//[$'\t\r\n ']/-}")
 fi
 
 PTH=$DIR/$NAME
 
 if [ $ADD_TIMESTAMP == 1 ]; then
-	PTH="$PTH"_$(date +"%Y-%m-%d_%H-%M-%S")
+    PTH="$PTH"_$(date +"%Y-%m-%d_%H-%M-%S")
 fi
 
 FILE_NAME="$PTH".png
-adb shell screencap -p | sed 's/\r$//' > $FILE_NAME
+# adb shell screencap -p | sed 's/\r$//' > $FILE_NAME
+adb shell screencap -p >$FILE_NAME
 
 FINAL_FILE_NAME=$FILE_NAME
 if [ $TO_JPG == 1 ]; then
-	FINAL_FILE_NAME="$PTH".jpg
-	convert $FILE_NAME $FINAL_FILE_NAME
-	rm $FILE_NAME
+    FINAL_FILE_NAME="$PTH".jpg
+    convert $FILE_NAME $FINAL_FILE_NAME
+    rm $FILE_NAME
 fi
 
 echo "$FINAL_FILE_NAME"
