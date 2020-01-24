d0c24e78dd66e50b35e8944a09fa3cf505fe6e7d
Yura Shinkarev <yshinkarev@gmail.com>
2020-01-24 12:53:21 +0300

    update postgres scripts

---
+++ _load_pg_props.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "${DB_PROP_FILE}" ]; then
+    DB_PROP_FILE=$(dirname "$0")/db.properties
+fi
+if [ ! -f ${DB_PROP_FILE} ]; then
+    >&2 echo "Missing DB properties file"
+    exit 1
+fi
+source ${DB_PROP_FILE}
